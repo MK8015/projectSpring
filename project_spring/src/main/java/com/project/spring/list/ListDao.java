cdca870e58701e59314a52121efd3ede96fc0b92
MK <masduck8015@gmail.com>
2023-01-04 18:58:29 +0900

    review 1차 완료

---
+++ project_spring/src/main/java/com/project/spring/list/ListDao.java
@@ -0,0 +1,38 @@
+package com.project.spring.list;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.project.spring.vo.PagingDto;
+import com.project.spring.vo.ProductVo;
+
+@Repository
+public class ListDao {
+	
+	private final String NAME_SPACE = "mappers.list.";
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	public List<ProductVo> getProductList(PagingDto pagingDto) {
+		return sqlSession.selectList(NAME_SPACE + "getProductList", pagingDto);
+	}
+	
+	public int getCount(PagingDto pagingDto) {
+		int count = sqlSession.selectOne(NAME_SPACE + "getCount", pagingDto);
+		return count;
+	}
+	
+	public List<ProductVo> getListByCategory(String category, PagingDto pagingDto){
+		Map<String, Object> map = new HashMap<>();
+		map.put("product_category", category);
+		map.put("startRow", pagingDto.getStartRow());
+		map.put("endRow", pagingDto.getEndRow());
+		return sqlSession.selectList(NAME_SPACE + "getListByCategory", map);
+	}
+}
